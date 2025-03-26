628ebbc77e44f8eab2b7bb102452074a026fad9b
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-03-26 10:42:39 +0300

    add vercel.json

--- eld-frontend/src/services/api.js
+++ eld-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://eld-backend-r63v.onrender.com';
 
 export const createTrip = async (tripData) => {
   const response = await axios.post(`${API_BASE_URL}/trips/`, tripData);
